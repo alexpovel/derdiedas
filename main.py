317c7aee2293ae3d7d69329fdb3ee04d4870540f
Alex Povel <git@alexpovel.de>
2022-12-11 11:33:52 +0100

    feat: Drop `FastAPI`, use `starlette` directly
    Also restructure static files, and drop
    JSON encoding from htmx, we can work with forms
    directly.

--- main.py
+++ main.py
@@ -1,64 +1,91 @@
 import json
 import random
 from http import HTTPStatus
+from typing import TypedDict
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, PlainTextResponse
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-
-app = FastAPI()
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, PlainTextResponse
+from starlette.routing import Mount, Route
+from starlette.staticfiles import StaticFiles
 
 Word = str
 Article = str
 
 
-class Term(BaseModel):
+class Term(TypedDict):
     article: Article
     word: Word
 
 
 with open("data/terms.json", encoding="utf-8") as f:
+    terms: list[Term] = json.load(f)
+
+    # For correctness checks:
     WORDS_TO_ARTICLES: dict[Word, Article] = {
-        item["word"]: item["article"] for item in json.load(f)
+        term["word"]: term["article"].casefold() for term in terms
     }
+
+    # For random picking:
     WORDS = list(WORDS_TO_ARTICLES.keys())
+
+    # For containment checks:
     ARTICLES = set(WORDS_TO_ARTICLES.values())
 
+with open("index.html", encoding="utf-8") as f:
+    index = f.read()
 
-@app.get("/word", response_class=PlainTextResponse)
-async def word() -> Word:
+
+def random_word() -> str:
     return random.choice(WORDS)
 
 
-@app.patch("/choice", response_class=HTMLResponse)
-async def choice(choice: Term) -> HTMLResponse:
-    current_word = choice.word
-    chosen_article = choice.article.lower()
+async def home(request: Request) -> HTMLResponse:
+    return HTMLResponse(content=index)
 
-    if current_word not in WORDS or chosen_article not in ARTICLES:
-        return HTMLResponse(status_code=HTTPStatus.BAD_REQUEST.value)
 
-    correct_article = WORDS_TO_ARTICLES[choice.word].lower()
+async def word(request: Request) -> PlainTextResponse:
+    return PlainTextResponse(content=random_word())
 
-    answered_correctly = chosen_article == correct_article
+
+async def choice(request: Request) -> HTMLResponse:
+    form = await request.form()
+
+    # `FormData` doesn't define `__match_args__`, so match more primitively for now.
+    match form.multi_items():
+        case [
+            ("article", str(a)),
+            ("word", str(w)),
+        ] if (article := a.casefold()) in ARTICLES and w in WORDS_TO_ARTICLES.keys():
+            pass  # We gucci
+        case _:
+            return HTMLResponse(status_code=HTTPStatus.BAD_REQUEST)
+
+    correct_article = WORDS_TO_ARTICLES[w]
+    answered_correctly = article == correct_article
 
     row = f"""
     <div class="articles">
-        <div class="wrong">
-            {'' if answered_correctly else chosen_article}
+        <div class="wrong{" hidden" if answered_correctly else ""}">
+            {article}
         </div>
         <div class="correct">
             {correct_article}
         </div>
     </div>
     <div class="chosen-word">
-        {current_word}
+        {w}
     </div>
-    <div id="word" hx-swap-oob="innerHTML">{await word()}</div>
+    <div id="word" hx-swap-oob="innerHTML">{random_word()}</div>
     """
     return HTMLResponse(content=row)
 
 
-# Catch the rest, needs to be defined last:
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+routes = [
+    Route("/", home, methods=["GET"]),
+    Route("/word", word, methods=["GET"]),
+    Route("/choice", choice, methods=["PATCH"]),
+    Mount("/static", StaticFiles(directory="static")),
+]
+
+app = Starlette(routes=routes)
